36f4915bcb8b59fa9d27b540d79b0bbb684abac7
mintkey <helendmpsy@gmail.com>
2020-07-26 13:36:39 -0400

    Updates

---
+++ precache-manifest.220a12f63cf748642c36fc7edf1ce92a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbeff8ca820e7d6458f8d4f105219425",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e4db234ab4efb62686f",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "6066bef55fd8a6cf575d",
+    "url": "/static/css/main.8276e2ed.chunk.css"
+  },
+  {
+    "revision": "3e4db234ab4efb62686f",
+    "url": "/static/js/2.7ab3c19b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7ab3c19b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6066bef55fd8a6cf575d",
+    "url": "/static/js/main.b3683f1f.chunk.js"
+  },
+  {
+    "revision": "766df7dbb5392e1314ed",
+    "url": "/static/js/runtime-main.9c9c41ef.js"
+  },
+  {
+    "revision": "beec581147086b7fb2ab2dc722e6d6a5",
+    "url": "/static/media/fordhamhacks.beec5811.png"
+  },
+  {
+    "revision": "2c13e5736e6f48202c2c9c3634396ab5",
+    "url": "/static/media/mathfest2020.2c13e573.png"
+  },
+  {
+    "revision": "e472aa283e7c0f712e30adb310e30bae",
+    "url": "/static/media/self-portrait.e472aa28.JPG"
+  }
+]);
