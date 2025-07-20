2031284361290ab5cf531c049a8b56097018c31a
mintkey <40031593+mintkey@users.noreply.github.com>
2025-07-20 14:05:22 -0400

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -14,8 +14,7 @@ const About = () => {
                     <br />
                     When I’m not working at <a href="https://nytimes.com/" target="_blank" rel="noopener noreferrer"><span>The New York Times</span></a>,
                     I can be found in a café working on a side project, somewhere in NY shooting
-                    film photography, at home playing <span>Final Fantasy</span>,
-                    and always listening to music.
+                    film photography, or training for the New York City Marathon.
                 </p>
             </Container>
         </>
